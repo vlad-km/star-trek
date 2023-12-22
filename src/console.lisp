affab67220ccf71e75ab174375bd7d49febdd8fa
vlad-km <gm5461@gmail.com>
2023-12-23 02:45:45 +0300

    Update console.lisp
    FIX

--- src/console.lisp
+++ src/console.lisp
@@ -1,145 +1,154 @@
 ;;; -*- mode:lisp; coding:utf-8  -*-
+#|
+
+            /\___/\
+            )     (
+           =\     /=                  if this code is not work, i dont know who wrote this code
+             )   (                    Copyright © 2017,2018,2023  @vlad-km
+            /     \                   2017, Original https://github.com/vlad-km/moren-electron
+            )     (                   2023, Code redesign
+           /       \                  Electron >= electron@21.2.2
+           \       /                  JSCL Moren edition 
+      jgs   \__ __/                   Node.js
+               ))
+              //
+             ((
+              \)
+|#
+
+;;; Star Trek widget
 
 
-(defvar *stc-header*)
-(defvar *stc-stardate*)
-(defvar *stc-location*)
-(defvar *stc-state*)
-(defvar *stc-command*)
-(defvar *stc-input*)
-(defvar *stc-output*)
-(defvar *so*)
-(defvar *stc-stream*)
-(defparameter *stc-bord* (viewport:make-border :style "groove" :width "1px" :radius "3px"))
-(defparameter *stc-back* (viewport:make-background :color "rgba(78, 78, 70, 0.431373)"))
+(defvar *stc-header* nil)
+(defvar *stc-stardate* nil)
+(defvar *stc-location* nil)
+(defvar *stc-uss* nil)
+(defvar *stc-state* nil)
+(defvar *stc-command* nil)
+(defvar *stc-input* nil)
+(defvar *stc-output* nil)
+(defvar *so* nil)
+(defvar *stc-stream* nil)
+(defvar *stc* nil)
 
-(defvar *stc*)
+(defparameter +stc-bord-css+
+  (css:inline '(:border-style "groove" :border-width "1px" :border-radius "3px")))
 
-(defun st-console-init()
-    (setq *stc-stardate*
-          (html:span
-           :id (klib:gen-uid "stc" "stardate")
-           :style "position:relative;width:20px;display:inline;"
-           :text "0000"))
+(defparameter +stc-back-css+
+  (css:inline  `(:background-color "rgba(78, 78, 70, 1.0)")))
+
+(defparameter +stc-stardate-css+
+  (css:inline `(:position "relative" :width "20px" :display "inline" :border "unset")))
+
+(defparameter +stc-location-css+
+  (css:inline `(:position "relative" :width "30px" :display "inline" :border "unset")))
+
+(defparameter +stc-uss-css+
+  (css:inline `(:position "relative" :width "50px" :display "inline" :border "unset")))
+
+
++stc-header-css+
+(defparameter +stc-header-css+
+  (css:inline `(:position "relative" :left "1px" :width "600px"
+                :border-style "groove" :border-width "1px" :border-radius "3px"
+                :background-color "#24502a" :color "#28c428"
+                :font-family "Consolas" :font-size "12px")))
+
+(defparameter +stc-input-css+
+  (css:inline `(:color "black" :background-color "rgba(232, 232, 87, 1.0)")))
+
+(defparameter +stc-output-css+
+  (css:inline  `(:position "relative" :left "1px" :width "600px" :height "390px"
+                 :overflow "auto"
+                 :color "#19ec34" :background-color "rgba(0, 0, 0, 1.0)"
+                 :font-family "Consolas" :font-size "13px")))
+
+
+(defun stc-console-init()
+  (setq *stc-uss*
+        (html:span :|id| "stc-uss-id"
+                   :style +stc-header-css+
+                   :append "  USS ENTERPRISE NCC-1701 command console "))
 
     (setq *stc-location*
-          (html:span
-           :id "stc-location-id"
-           :style "position:relative;width:30px;display:inline;"
-           :text "| STARBASE DOCK |"))
+        (html:span  :|id| "stc-location-id"  :style +stc-location-css+
+                    :append "|     STARBASE DOCK      |"))
 
-    (setq *stc-header*
-          (html:div
-           :id (klib:gen-uid "stc" "header")
-           :style (concat
-                   "position:relative;left:1px;width:600px;"
-                   "border-style:groove;border-width:1px;border-radius:3px;"
-                   "background-color:rgba(0,0,0,0.8);color:green")
-           :text "USS ENTERPRISE command console | NCC-1701 "))
+  (setq *stc-stardate*
+        (html:span  :|id| "stc-stardate-id"  :style +stc-stardate-css+
+                    :append  "0000"))
 
-    (dom:mount *stc-header*
-               *stc-location*
-               " STARDATE: "
-               *stc-stardate*)
+  (setq *stc-header*
+        (html:div :|id| "stc-header-id"
+                  :style +stc-header-css+
+                  :append *stc-uss*
+                  :append *stc-location*
+                  :append (html:span :append " STARDATE ")
+                  :append *stc-stardate*
+                  :append " |"))
 
-    (setq *stc-command*
-          (html:div
-           :id (klib:gen-uid "stc" "command")
-           :style
-           (concat
-            ;;"display:inline-block;"
-            "background-color:rgba(0, 0, 0, 0.8);")))
+  (setq *stc-command*
+        (html:div  :|id| "stc-command-id"
+                   :style (css:inline '(:background-color "rgba(0, 0, 0, 1.0)"
+                                        :font-family "Consolas"
+                                        :font-szie "13px" :color "#19ec34" ))))
 
-    (setq *stc-state*
-          (html:div
-           :id (klib:gen-uid "stc" "state")
-           :style
-           (concat
-            ;;"display:inline-block;"
-            "width:" "40px;"
-            "font-family:Consolas;"
-            "font-size:13px;"
-            "color:" "#19ec34" )
-           :text "COMMAND:"))
-
-    (setq *stc-input*
-          (html:textarea
-           :id (concat (string (gensym "talk")) "-id")
-           :color "black"
-           :style "background-color:rgba(232, 232, 87, 0.38);"
-           :rows "1"
-           ;;:cols "120"
-           :spellcheck "false"))
-
-    (dom:mount *stc-command* *stc-state* *stc-input*)
-
-    (setq *stc-output*
-          (html:div
-           :id (klib:gen-uid "stc" "output")
-           :style
-           (concat
-            "position:relative;left:1px;width:600px;height:390px;overflow:auto;"
-            "background-color:rgba(0, 0, 0, 0.8);"
-            "font-family:Consolas;"
-            "font-size:13px;"
-            "color:" "#19ec34")))
-
-    (setq *stc-stream*
-          (dom:make-dom-output-stream
-           :name (concat (string (gensym "stream")) "-id")))
-
-    (setq *so* *stc-stream*)
-
-    (dom:output-stream-open *stc-stream* *stc-output*)
+  (setq *stc-state*
+        (html:div :|id| "stc-state-id"
+                  :style  (css:inline `(:width "40px" :color "black"
+                                        :font-family "Consolas"
+                                        :font-szie "13px" :color "#19ec34"))))
+  (setq *stc-input*
+        (html:textarea :|id| "stc-talk-id"
+                       :style +stc-input-css+ 
+                       :|rows| "1"
+                       :|spellcheck| "false"))
 
 
-    (setq *stc*
-          (viewport:create :left 500
-                           :top 50
-                           :height 400
-                           :width 600
-                           :drag t
-                           :border *stc-bord*
-                           :background *stc-back*
-                           :childs '(*stc-header* *stc-output* *stc-command*)
-                           :parent dom:*body*))
+  (setq *stc-output*
+        (html:div :|id| "stc-stream-id"  :style +stc-output-css+))
+  
 
-    (setq *stc-location* (dom:get-element-by-id "stc-location-id"))
-
-    (jq:on (jq:$ *stc-input*) "keyup"
-           (lambda (event) (stc/kb-handler event))) )
+  (setq *stc*
+        (html:div :|id| "stc-console"
+                  :|style| (css:inline `(:position "absolute" :left "500px" :top "50px"
+                                         :height "400px" :width "600px"
+                                         :border ,+stc-bord-css+
+                                         :background ,+stc-back-css+))
+                  :append *stc-header*
+                  :append *stc-output*
+                  :append *stc-input*
+                  :append-to (html:document-body)))
+  
+  (let ((pvp (#j:$ *stc* )))
+    (funcall ((jscl::oget pvp "draggable" "bind") pvp)))
+  nil)
 
 
 ;;; hide
-(defun stc/hide ()
-    (viewport:hide *stc*))
-
+(defun stc/hide () (ffi:setprop (*stc* "display") "none"))
 ;;; show
-(defun stc/show ()
-    (viewport:show *stc*))
-
+(defun stc/show () (ffi:setprop (*stc* "display") ""))
+(defun %set-inner-html (el data)  (ffi:setprop (el "innerText") data))
 ;;; display quad name
-(defun stc/quad (name)
-    (dom:set-inner-html *stc-location* (concat "| " name " |")))
-
+(defun stc/quad (name) (%set-inner-html *stc-location* (jscl::concat "|  " name "  |")))
 ;;; display stardate
-(defun stc/stardate (date)
-    (dom:set-inner-html *stc-stardate* (roundnum date 4)))
+(defun stc/stardate (date)(%set-inner-html *stc-stardate* (roundnum date 4)))
 
 
 ;;; terpri
-(defun stc/terpri ()
+#+nil (defun stc/terpri ()
     (format *so* "~%"))
 
 ;;; clear console
-(defun stc/clear ()
-    (dom:output-stream-reset *stc-stream*))
+#+nil (defun stc/clear ()
+    (output-stream-reset *stc-stream*))
 
 
-(defparameter reg-del (reg-exp "[\\n\\r]" "g"))
+#+nil (defparameter reg-del (reg-exp "[\\n\\r]" "g"))
 
 ;;; reader
-(defun stc/kb-handler (evt)
+#+nil (defun stc/kb-handler (evt)
     (let ((el (oget evt "target"))
           (data))
         (cond ((equal (oget evt "keyCode") 13)
@@ -152,7 +161,7 @@
     (values) )
 
 
-(defun stc/kbd-reader (data)
+#+nil (defun stc/kbd-reader (data)
     (let ((stream (jscl::make-string-stream data))
           (sentinel (gensym "EOF"))
           (s)
@@ -165,7 +174,5 @@
            (go feeder)
          rdr-eof)
         (reverse res)))
-
-
 
 ;;; EOF
