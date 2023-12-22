b566bb9e6971fa05413335f13fe2165d07a7f1aa
vlad-km <gm5461@gmail.com>
2023-12-22 21:52:39 +0300

    Update fsm.lisp

--- src/fsm.lisp
+++ src/fsm.lisp
@@ -1,12 +1,23 @@
 ;;; -*- mode:lisp; coding:utf-8  -*-
+#|
+
+            /\___/\
+            )     (
+           =\     /=                  if this code is not work, i dont know who wrote this code
+             )   (                    Copyright © 2017,2018,2023  @vlad-km
+            /     \                   2017, Original https://github.com/vlad-km/moren-electron
+            )     (                   2023, Code redesign
+           /       \                  Electron >= electron@21.2.2
+           \       /                  JSCL Moren edition 
+      jgs   \__ __/                   Node.js
+               ))
+              //
+             ((
+              \)
+|#
 
 
 (defvar *state-fsm*)
-
-(defun state (lbl)
-    (setq *state-fsm* lbl))
-
-
 (defvar *c1*)
 (defvar *w1*)
 (defvar *n*)
@@ -15,125 +26,114 @@
 (defvar *x1*)
 (defvar *y1*)
 
+(defun state (lbl)
+    (setq *state-fsm* lbl))
 
 (mordev:rx-listen :fsm (lambda (data) (fsm data)))
 
 (defun fsm (input)
-
-    (case *state-fsm*
-
-      (:accept-command
-       (when (eql (car input) 'n)
-           (end-of-game)
-           (return-from fsm (values)))
-       (enter-quad)
-       (mloop))
-
-      (:mloop-command
-       (mloop-command (car input))
+  (case *state-fsm*
+    (:accept-command
+     (when (eql (car input) 'n)
+       (end-of-game)
        (return-from fsm (values)))
-
-      (:navigate
-       (input-course-message "LT. SULU")
-       (state :input-course-check)
-       (return-from fsm (values)))
-
-      (:input-course-check
-       (setq *c1* (input-course-check (car input)))
-       (when *c1*
-           (format *so* ": ~a " *new-course*)
-           (nav-factor-message (if (< (aref *ddd* 1) 0) 0.2 8))
-           (state :nav-factor))
-       (return-from fsm (values)))
-
-      (:nav-factor
-       (state :mloop-command)
-       (setq *w1* (nav-factor (car input)))
-       (when *w1*
-           (format *so* ": ~a~%" *w1*)
-           (setq *n* (nav-energy *w1*))
-           (unless *n* (return-from fsm (values)))
-           (klingon-attack-warp)
-           (repair-by-warp *w1*)
-           (damage-by-warp)
-           (when (not (nav4 *new-course* *n* *w1*))
-               (return-from fsm t))
-           (warp-time *w1*))
-       ;;(state :mloop-command)
-       (return-from fsm (values)))
-
-      (:torpedo-course
-       ;;(state :mloop-command)
-       (setq *c1* (input-course-check (car input)))
-       (when *c1*
-           (format *so* ": ~a~%" *new-course*)
-           (decf *energy* 2)
-           (decf *torpedo* 1)
-           (torpedo-fire *new-course*)
-           (setq *klingon-attack* t))
-       (state :mloop-command)
-       (return-from fsm (values)))
-
-      (:phaser
-       (format *so* ": ~a~%" input)
-       (phaser4 (car input))
-       (state :mloop-command)
-       (return-from fsm (values)))
-
-      (:shield
-       (format *so* ": ~a~%" input)
-       (shield (car input))
-       (state :mloop-command)
-       (return-from fsm (values)))
-
-      (:computer
-       (computer (car input))
-       ;;(state :mloop-command)
-       (return-from fsm (values)))
-
-      (:comp-calc
-       (format *so* ": ~a~%" input)
-       (setq *x0* (car input))
-       (setq *y0* (cadr input))
-       (format *so* "FINAL COORDINATES X Y?")
-       (state :comp-calc-final-co)
-       (return-from fsm (values)))
-
-      (:comp-calc-y
-       (setq *y0* input)
-       (format t "FINAL COORDINATES X?")
-       (state :comp-calc-final-x)
-       (return-from fsm (values)))
-
-      (:comp-calc-final-x
-       (setq *x1* input)
-       (format t "Y?")
-       (state :comp-calc-final-y)
-       (return-from fsm (values)))
-
-      (:comp-calc-final-co
-       (format *so* ": ~a~%" input)
-       (setq *x1* (car input))
-       (setq *y1* (cadr input))
-       (disp-direct-dist *x0* *y0* *x1* *y1*)
-       (state :mloop-command)
-       (return-from fsm (values)))
-
-      (:need-repair
-       (format *so* "~a~%" input)
-       (cond ((eql 'y (car input))
-              (repair-all)
-              (incf *time* (+ *d3-repair* 0.1))
-              (show-stat-repair)))
-       (state :mloop-command)
-       (return-from fsm (values)))
-
-      (:more-mission
-       (if (eql (car input) 'aye)
-           (mordev:rx-emit :new-mission))
-       (return-from fsm (values)))
-
-      ))
+     (enter-quad)
+     (mloop))
+    (:mloop-command
+     (mloop-command (car input))
+     (return-from fsm (values)))
+    (:navigate
+     (input-course-message "LT. SULU")
+     (state :input-course-check)
+     (return-from fsm (values)))
+    (:input-course-check
+     (setq *c1* (input-course-check (car input)))
+     (when *c1*
+       (display ": ~a " *new-course*)
+       (nav-factor-message (if (< (aref *ddd* 1) 0) 0.2 8))
+       (state :nav-factor))
+     (return-from fsm (values)))
+    (:nav-factor
+     (state :mloop-command)
+     (setq *w1* (nav-factor (car input)))
+     (when *w1*
+       (display ": ~a~%" *w1*)
+       (setq *n* (nav-energy *w1*))
+       (unless *n* (return-from fsm (values)))
+       (klingon-attack-warp)
+       (repair-by-warp *w1*)
+       (damage-by-warp)
+       (when (not (nav4 *new-course* *n* *w1*))
+         (return-from fsm t))
+       (warp-time *w1*))
+     ;;(state :mloop-command)
+     (return-from fsm (values)))
+    (:torpedo-course
+     ;;(state :mloop-command)
+     (setq *c1* (input-course-check (car input)))
+     (when *c1*
+       (display ": ~a~%" *new-course*)
+       (decf *energy* 2)
+       (decf *torpedo* 1)
+       (torpedo-fire *new-course*)
+       (setq *klingon-attack* t))
+     (state :mloop-command)
+     (return-from fsm (values)))
+    (:phaser
+     (display ": ~a~%" input)
+     (phaser4 (car input))
+     (state :mloop-command)
+     (return-from fsm (values)))
+    (:shield
+     (display ": ~a~%" input)
+     (shield (car input))
+     (state :mloop-command)
+     (return-from fsm (values)))
+    (:computer
+     (computer (car input))
+     ;;(state :mloop-command)
+     (return-from fsm (values)))
+    (:comp-calc
+     (display ": ~a~%" input)
+     (setq *x0* (car input))
+     (setq *y0* (cadr input))
+     (display "FINAL COORDINATES X Y?")
+     (state :comp-calc-final-co)
+     (return-from fsm (values)))
+    (:comp-calc-y
+     (setq *y0* input)
+     ;; note: wtf?
+     ;; note: the state never use
+     (format t "FINAL COORDINATES X?")
+     (state :comp-calc-final-x)
+     (return-from fsm (values)))
+    (:comp-calc-final-x
+     (setq *x1* input)
+     ;; note: wtf?
+     ;; note: the state never use
+     (format t "Y?")
+     (state :comp-calc-final-y)
+     (return-from fsm (values)))
+    (:comp-calc-final-co
+     (display ": ~a~%" input)
+     (setq *x1* (car input))
+     (setq *y1* (cadr input))
+     (disp-direct-dist *x0* *y0* *x1* *y1*)
+     (state :mloop-command)
+     (return-from fsm (values)))
+    (:need-repair
+     (display "~a~%" input)
+     (cond ((eql 'y (car input))
+            (repair-all)
+            (incf *time* (+ *d3-repair* 0.1))
+            (show-stat-repair)))
+     (state :mloop-command)
+     (return-from fsm (values)))
+    (:more-mission
+     (if (eql (car input) 'aye)
+         (mordev:rx-emit :new-mission))
+     (return-from fsm (values)))
+    ))
 
 
 
