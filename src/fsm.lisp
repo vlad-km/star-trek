90aa8924720c00d7ee0187d8fcca52896c1eda31
vlad-km <gm5461@gmail.com>
2023-12-27 16:42:14 +0300

    Update fsm.lisp

--- src/fsm.lisp
+++ src/fsm.lisp
@@ -159,133 +159,157 @@
 |#
 
 ;;; macro for deref ((data)) -> (data)
-(defmacro @fsm-data () `(setq data (car data)))
+;;;(defmacro @fsm-data () `(setq data (car data)))
 
 (defvar *fsm-pgm
   (list
+
    (@def-pgm :accept-command
-             (@clt "FSM :accept-command" data)
-             (@fsm-data)
-             (cond ((eql (car data) 'n)
-                    (end-of-game))
-                   (t (enter-quad)
-                      (mloop))))
+       (@clt "FSM" data)
+     ;;(#j:console:log "DATA" data)
+     ;;(@fsm-data)
+     (cond ((eql (car data) 'n)
+            (end-of-game))
+           (t (enter-quad)
+              (mloop))))
+
+   (@def-pgm :mloop-command
+       (@clt "FSM" data)
+     ;;(#j:console:log "data" data)
+     (@fsm-data)
+     (mloop-command data)
+     (values))
+   
    (@def-pgm :navigate
-             (input-course-message "LT. SULU")
-             (state :input-course-check)
-             (values))
+       (input-course-message "LT. SULU")
+     (state :input-course-check)
+     (values))
+
    (@def-pgm :input-course-check
-             (@fsm-data)
-             (@clt "FSM :input-course-check" data)
-             (setq *c1* (input-course-check (car data)))
-             (when *c1*
-               (display ": ~a " *new-course*)
-               (nav-factor-message (if (< (aref *ddd* 1) 0) 0.2 8))
-               (state :nav-factor))
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (setq *c1* (input-course-check (car data)))
+     (when *c1*
+       (display ": ~a " *new-course*)
+       (nav-factor-message (if (< (aref *ddd* 1) 0) 0.2 8))
+       (state :nav-factor))
+     (values))
+
    (@def-pgm :nav-factor
-             (@fsm-data)
-             (@clt "FSM :nav-factor" data)
-             (state :mloop-command)
-             (setq *w1* (nav-factor (car data)))
-             (when *w1*
-               (display ": ~a~%" *w1*)
-               (setq *n* (nav-energy *w1*))
-               (when *n* 
-                 (klingon-attack-warp)
-                 (repair-by-warp *w1*)
-                 (damage-by-warp)
-                 (cond  ((not (nav4 *new-course* *n* *w1*)) nil)
-                        (t (warp-time *w1*)))))
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (state :mloop-command)
+     (setq *w1* (nav-factor (car data)))
+     (when *w1*
+       (display ": ~a~%" *w1*)
+       (setq *n* (nav-energy *w1*))
+       (when *n* 
+         (klingon-attack-warp)
+         (repair-by-warp *w1*)
+         (damage-by-warp)
+         (cond  ((not (nav4 *new-course* *n* *w1*)) nil)
+                (t (warp-time *w1*)))))
+     (values))
+
    (@def-pgm :torpedo-course
-             (@fsm-data)
-             (@clt "FSM :torpedo-course" data)
-             (setq *c1* (input-course-check (car data)))
-             (when *c1*
-               (display ": ~a~%" *new-course*)
-               (decf *energy* 2)
-               (decf *torpedo* 1)
-               (torpedo-fire *new-course*)
-               (setq *klingon-attack* t))
-             (state :mloop-command)
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (setq *c1* (input-course-check (car data)))
+     (when *c1*
+       (display ": ~a~%" *new-course*)
+       (decf *energy* 2)
+       (decf *torpedo* 1)
+       (torpedo-fire *new-course*)
+       (setq *klingon-attack* t))
+     (state :mloop-command)
+     (values))
+
    (@def-pgm :phaser
-             (@fsm-data)
-             (@clt "FSM :phaser" data)
-             (display ": ~a~%" data)
-             (phaser4 (car data))
-             (state :mloop-command)
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (display ": ~a~%" data)
+     (phaser4 (car data))
+     (state :mloop-command)
+     (values))
+
    (@def-pgm :shield
-             (@fsm-data)
-             (@clt "FSM :shield" data)
-             (display ": ~a~%" data)
-             (shield (car data))
-             (state :mloop-command)
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (display ": ~a~%" data)
+     (shield (car data))
+     (state :mloop-command)
+     (values))
+
    (@def-pgm :computer
-             (@fsm-data)
-             (@clt "FSM :computer" data)
-             (computer (car data))
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (computer (car data))
+     (values))
+
    (@def-pgm :comp-calc
-             (@fsm-data)
-             (@clt "FSM :comp-calc" data)
-             (display ": ~a~%" data)
-             (setq *x0* (car data))
-             (setq *y0* (cadr data))
-             (display "FINAL COORDINATES X Y?")
-             (state :comp-calc-final-co)
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (display ": ~a~%" data)
+     (setq *x0* (car data))
+     (setq *y0* (cadr data))
+     (display "FINAL COORDINATES X Y?")
+     (state :comp-calc-final-co)
+     (values))
+
    (@def-pgm :comp-calc-y
-             (@fsm-data)
-             (setq *y0* data)
-             ;; note: wtf?
-             ;; note: the state never use
-             (format t "FINAL COORDINATES X?")
-             (state :comp-calc-final-x)
-             (values))
+       (@fsm-data)
+     (setq *y0* data)
+     ;; note: wtf?
+     ;; note: the state never use
+     (format t "FINAL COORDINATES X?")
+     (state :comp-calc-final-x)
+     (values))
+
    (@def-pgm :comp-calc-final-x
-             (@fsm-data)
-             (setq *x1* data)
-             ;; note: wtf?
-             ;; note: the state never use
-             (format t "Y?")
-             (state :comp-calc-final-y)
-             (values))
+       (@fsm-data)
+     (setq *x1* data)
+     ;; note: wtf?
+     ;; note: the state never use
+     (format t "Y?")
+     (state :comp-calc-final-y)
+     (values))
+
    (@def-pgm :comp-calc-final-co
-             (@fsm-data)
-             (display ": ~a~%" data)
-             (@clt "FSM :comp-calc-final-co" data)
-             (setq *x1* (car data))
-             (setq *y1* (cadr data))
-             (disp-direct-dist *x0* *y0* *x1* *y1*)
-             (state :mloop-command)
-             (values))
+       (@fsm-data)
+     (display ": ~a~%" data)
+     (@clt "FSM" data)
+     (setq *x1* (car data))
+     (setq *y1* (cadr data))
+     (disp-direct-dist *x0* *y0* *x1* *y1*)
+     (state :mloop-command)
+     (values))
+
    (@def-pgm :need-repair
-             (@clt "FSM :need-repair" data)
-             (@fsm-data)
-             (display "~a~%" data)
-             (cond ((eql 'y (car data))
-                    (repair-all)
-                    (incf *time* (+ *d3-repair* 0.1))
-                    (show-stat-repair)))
-             (state :mloop-command)
-             (values))
+       (@clt "FSM" data)
+     (@fsm-data)
+     (display "~a~%" data)
+     (cond ((eql 'y (car data))
+            (repair-all)
+            (incf *time* (+ *d3-repair* 0.1))
+            (show-stat-repair)))
+     (state :mloop-command)
+     (values))
+
    (@def-pgm :more-mission
-             (@fsm-data)
-             (@clt "FSM :more-mission" data)
-             (if (eql (car data) 'bye)
-                 (rx:emit :new-mission nil))
-             (values))
+       (@fsm-data)
+     (@clt "FSM" data)
+     (if (eql (car data) 'bye)
+         (rx:emit :new-mission nil))
+     (values))
    ))
 
 (defun fsm (data)
-  (@clt "FSM-receive" data)
+  ;;(@fsm-data)
+  (@clt "FSM receive:" data)
+  ;;(#j:console:log "DATA" data)
   (funcall (@exec-pgm *fsm-pgm
                       *state-fsm*
-                      (lambda (&optional x) (error "FSM ABEND ~a ~a" *state-fsm* data)))
+                      (lambda (&optional x) (error "FSM ABEND ~a ~a~&" *state-fsm* data)))
            data))
 
 
